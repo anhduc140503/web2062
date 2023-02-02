ee3abb1232f34dc31395246c3ba51dba101310db
anhduc140503 <anhduchtri8888@gmail.com>
2023-02-02 17:03:28 +0700

    update

--- session9/challenge1/index.js
+++ session9/challenge1/index.js
@@ -1,4 +1,41 @@
 const game={
-
-    
+team1: 'Bayern Munich',
+team2: 'Borrussia Dortmund',
+player:[
+[
+    'Neuer',
+    'Sule',
+    'Pavart',
+    'Kimmich',
+    'Martinez',
+    'Lewandowski',
+    'Davies',
+    'Hernandez',
+    'Tolisso',
+    'Muller',
+    'Coman',
+],
+[
+'Kobel',
+'Schulz',
+'Ozcan',
+'Haller',
+'Wolf',
+'Brandt',
+'Bellingham',
+'Can',
+'Hummels',
+'Ryerson',
+'Adeyemi',
+],
+],
+score: '4:0',
+scored:['Davies', 'Muller', 'Lewandowski' ,'Kimmich'],
+date: 'May 14th, 2003',
+odds:{
+   team1:1.33,//ti le thang cua bayem
+   x:3.25, // ti le hoa
+   team2:6.5, // ti le thang cua dortmund
 }
+
+};
